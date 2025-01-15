b70a1d6097950dc3d20e965c14873e0006d0ca91
toastkidjp <10fmi13@gmail.com>
2025-01-16 07:44:46 +0900

    refactor Add licenses.

--- docs/static/script/converter.js
+++ docs/static/script/converter.js
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2025 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+
+/**
+ * @author toastkidjp
+ */
 function encode() {
     const input = document.getElementById('input');
     const encodedTextValue = encodeURIComponent(input.value);
