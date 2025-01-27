9da4a98f38b9e1796163198d0aac2aeee379051a
toastkidjp <10fmi13@gmail.com>
2025-01-27 09:47:35 +0900

    feature Re-order timezones.

--- assets/script/worldTime.js
+++ assets/script/worldTime.js
@@ -10,22 +10,22 @@
  * @author toastkidjp
  */
 const timezones = [
-  { name: "Asia/Tokyo", icon: "\uD83C\uDDEF\uD83C\uDDF5" },
-  { name: "UTC", icon: "" },
-  { name: "America/New_York", icon: "\uD83C\uDDFA\uD83C\uDDF8" },
-  { name: "US/Pacific", icon: "\uD83C\uDDFA\uD83C\uDDF8" },
-  { name: "US/Hawaii", icon: "\uD83C\uDDFA\uD83C\uDDF8" },
-  { name: "Europe/Rome", icon: "\uD83C\uDDEE\uD83C\uDDF9" },
-  { name: "Australia/Sydney", icon: "\uD83C\uDDE6\uD83C\uDDFA" },
   { name: "NZ", icon: "\uD83C\uDDF3\uD83C\uDDFF" },
-  { name: "Asia/Ho_Chi_Minh", icon: "\uD83C\uDDFB\uD83C\uDDF3" },
+  { name: "Australia/Sydney", icon: "\uD83C\uDDE6\uD83C\uDDFA" },
+  { name: "Asia/Tokyo", icon: "\uD83C\uDDEF\uD83C\uDDF5" },
+  { name: "Pacific/Palau", icon: "\uD83C\uDDF5\uD83C\uDDFC" },
   { name: "Asia/Ulaanbaatar", icon: "\uD83C\uDDF2\uD83C\uDDF3" },
   { name: "Asia/Almaty", icon: "\uD83C\uDDF0\uD83C\uDDFF" }, 
   { name: "Asia/Tbilisi", icon: "\uD83C\uDDEC\uD83C\uDDEA" },
+  { name: "Asia/Ho_Chi_Minh", icon: "\uD83C\uDDFB\uD83C\uDDF3" },
   { name: "Africa/Johannesburg", icon: "\uD83C\uDDFF\uD83C\uDDE6" },
+  { name: "Europe/Rome", icon: "\uD83C\uDDEE\uD83C\uDDF9" },
+  { name: "UTC", icon: "" },
   { name: "America/Asuncion", icon: "\uD83C\uDDF5\uD83C\uDDFE" },
   { name: "America/Buenos_Aires", icon: "\uD83C\uDDE6\uD83C\uDDF7" },
-  { name: "Pacific/Palau", icon: "\uD83C\uDDF5\uD83C\uDDFC" },
+  { name: "America/New_York", icon: "\uD83C\uDDFA\uD83C\uDDF8" },
+  { name: "US/Pacific", icon: "\uD83C\uDDFA\uD83C\uDDF8" },
+  { name: "US/Hawaii", icon: "\uD83C\uDDFA\uD83C\uDDF8" },
 ];
 
 function makeTimeContent() {
