9d8f20fe5fd570dca62ed9fa55f8101df595dff7
toastkidjp <10fmi13@gmail.com>
2025-01-30 00:07:09 +0900

    refactor Modify indents.

--- assets/script/loan.js
+++ assets/script/loan.js
@@ -10,31 +10,31 @@
  * @author toastkidjp
  */
 function calculate() {
-    const paymentCount = (document.getElementById("term").value * 12);
-    const convertedRate = document.getElementById("interestRate").value / 100.0;
-    const actualTotalAmount = document.getElementById("amount").value - document.getElementById("downPayment").value;
-    let currentAmount = actualTotalAmount;
-    const poweredMonthlyInterestRate = Math.pow((1 + convertedRate / 12), paymentCount);
+  const paymentCount = (document.getElementById("term").value * 12);
+  const convertedRate = document.getElementById("interestRate").value / 100.0;
+  const actualTotalAmount = document.getElementById("amount").value - document.getElementById("downPayment").value;
+  let currentAmount = actualTotalAmount;
+  const poweredMonthlyInterestRate = Math.pow((1 + convertedRate / 12), paymentCount);
 
-    const numerator = (actualTotalAmount * convertedRate) / 12 * poweredMonthlyInterestRate;
-    const denominator = poweredMonthlyInterestRate - 1;
-    const monthlyPaymentToBank = Math.max(numerator / denominator, 0.0);
+  const numerator = (actualTotalAmount * convertedRate) / 12 * poweredMonthlyInterestRate;
+  const denominator = poweredMonthlyInterestRate - 1;
+  const monthlyPaymentToBank = Math.max(numerator / denominator, 0.0);
 
-    let wholeInterest = 0;
+  let wholeInterest = 0;
 
-    let newTable = '<table cellpadding="0" cellspacing="0" border="0" width="100%">'
-      + '<th>Amount</th><th>Actual returning</th><th>Interest</th>';
-    for (let i = 0; i < paymentCount; i++) {
-        const monthlyInterest = currentAmount * (convertedRate / 12);
-        wholeInterest += monthlyInterest;
-        const monthlyActualReturning = monthlyPaymentToBank - monthlyInterest;
-        currentAmount -= (monthlyPaymentToBank - monthlyInterest);
-        newTable += '<tr><td>' + Math.round(currentAmount).toLocaleString() + '</td><td>' + Math.round(monthlyActualReturning).toLocaleString() + '</td><td>' + Math.round(monthlyInterest).toLocaleString() + '</td></tr>';
-    }
-    newTable += '</table>';
+  let newTable = '<table cellpadding="0" cellspacing="0" border="0" width="100%">'
+    + '<th>Amount</th><th>Actual returning</th><th>Interest</th>';
+  for (let i = 0; i < paymentCount; i++) {
+    const monthlyInterest = currentAmount * (convertedRate / 12);
+    wholeInterest += monthlyInterest;
+    const monthlyActualReturning = monthlyPaymentToBank - monthlyInterest;
+    currentAmount -= (monthlyPaymentToBank - monthlyInterest);
+    newTable += '<tr><td>' + Math.round(currentAmount).toLocaleString() + '</td><td>' + Math.round(monthlyActualReturning).toLocaleString() + '</td><td>' + Math.round(monthlyInterest).toLocaleString() + '</td></tr>';
+  }
+  newTable += '</table>';
 
-    document.getElementById('paymentSchedule').innerHTML = newTable;
+  document.getElementById('paymentSchedule').innerHTML = newTable;
 
-    const monthlyPayment = monthlyPaymentToBank + document.getElementById("managementFee").value + document.getElementById("renovationReserves").value;
-    document.getElementById('monthlyPayment').textContent = "Monthly payment: " + Math.round(monthlyPayment).toLocaleString() + " (Interest: " + Math.round(wholeInterest).toLocaleString() + ")";
+  const monthlyPayment = monthlyPaymentToBank + document.getElementById("managementFee").value + document.getElementById("renovationReserves").value;
+  document.getElementById('monthlyPayment').textContent = "Monthly payment: " + Math.round(monthlyPayment).toLocaleString() + " (Interest: " + Math.round(wholeInterest).toLocaleString() + ")";
 }
