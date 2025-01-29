e68859e0d54b85c57fd16c1f8ec4558f6df27a06
toastkidjp <10fmi13@gmail.com>
2025-01-30 00:07:19 +0900

    refactor Modify indents.

--- assets/script/converter.js
+++ assets/script/converter.js
@@ -10,43 +10,43 @@
  * @author toastkidjp
  */
 function sanitize(input) {
-    return input.replaceAll('<', '').replaceAll('>', '');
+  return input.replaceAll('<', '').replaceAll('>', '');
 }
 
 function encode() {
-    const input = document.getElementById('input');
-    const encodedTextValue = encodeURIComponent(input.value);
-    document.getElementById('encoded').value = sanitize(encodedTextValue);
+  const input = document.getElementById('input');
+  const encodedTextValue = encodeURIComponent(input.value);
+  document.getElementById('encoded').value = sanitize(encodedTextValue);
 }
 
 function decode() {
-    const encoded = document.getElementById('encoded');
-    try {
-        const decodedTextValue = decodeURIComponent(encoded.value);
-        document.getElementById('input').value = sanitize(decodedTextValue);
-    } catch (e) {
-        // > /dev/null
-    }
+  const encoded = document.getElementById('encoded');
+  try {
+    const decodedTextValue = decodeURIComponent(encoded.value);
+    document.getElementById('input').value = sanitize(decodedTextValue);
+  } catch (e) {
+    // > /dev/null
+  }
 }
 
 function setInitialTime() {
-    const now = Date.now();
-    document.getElementById('unixtime').value = now;
-    toDateTime();
+  const now = Date.now();
+  document.getElementById('unixtime').value = now;
+  toDateTime();
 }
 
 function toDateTime() {
-    const input = document.getElementById('unixtime');
-    const encodedTextValue = new Date(Number(input.value));
-    document.getElementById('datetime').value = sanitize(encodedTextValue.toLocaleString());
+  const input = document.getElementById('unixtime');
+  const encodedTextValue = new Date(Number(input.value));
+  document.getElementById('datetime').value = sanitize(encodedTextValue.toLocaleString());
 }
 
 function toUnixTime() {
-    const encoded = document.getElementById('datetime');
-    try {
-        const decodedTextValue = Date.parse(encoded.value);
-        document.getElementById('unixtime').value = decodedTextValue;
-    } catch (e) {
-        // > /dev/null
-    }
+  const encoded = document.getElementById('datetime');
+  try {
+    const decodedTextValue = Date.parse(encoded.value);
+    document.getElementById('unixtime').value = decodedTextValue;
+  } catch (e) {
+    // > /dev/null
+  }
 }
