c664e3fca18fa29a3754697003881a5b7950b092
toastkidjp <10fmi13@gmail.com>
2025-02-22 10:13:54 +0900

    refactor Modify interfaces.

--- assets/script/today.js
+++ assets/script/today.js
@@ -29,5 +29,5 @@ function generateTodayArticleLink() {
 
   const title = monthLabel[date.getMonth()] + "_" + date.getDate();
   const link = "https://en.wikipedia.org/wiki/" + title;
-  document.getElementById("link_to_article").innerHTML = "<h3>What day is it?</h3><a href='" + link + "'>" + title + " (Wikipedia)</a>";
+  return "<h3>What day is it?</h3><a href='" + link + "'>" + title + " (Wikipedia)</a>";
 }
